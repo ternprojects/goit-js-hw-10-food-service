eb60c6bf2caa201b0996855f6233a9edba89971c
Y.Tern <ternxstyle@gmail.com>
2020-06-12 01:21:46 +0300

    add deploy

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import './styles.css';
 import menu from './menu.json'
 
 const list = document.querySelector('.js_list');
-const allLi = menu.map(item => `<li>Name: ${item.name}</li>`).join('');
-list.insertAdjacentHTML('beforeend', allLi);
+
+/* Шаблонная строка */
+//const allLi = menu.map(item => `<li>Name: ${item.name}</li>`).join('');
+//list.insertAdjacentHTML('beforeend', allLi);
+
 
